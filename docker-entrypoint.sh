1bdf9655686aa2a2088124249eeaea45698fdcc5
pratap <pratapvarmad.19@gmail.com>
2026-06-07 17:23:26 -0400

    NR key update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 # Inject the New Relic license key at container start.
 # NR_LICENSE_KEY comes from docker-compose environment (sourced from .env on the host).
 # If unset the agent stays disabled (licenseKey empty → main.ts skips init).
-NR_KEY="${NR_LICENSE_KEY:-}"
+NR_KEY="${NR_BROWSER_LICENSE_KEY:-}"
 sed "s|__NR_LICENSE_KEY__|${NR_KEY}|g" \
   /templates/nr-config.js.template \
   > /usr/share/nginx/html/nr-config.js
