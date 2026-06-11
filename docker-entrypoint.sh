c61fa42bb1036758c5030461d0a0ff8f525e8796
pratap <pratapvarmad.19@gmail.com>
2026-06-11 12:26:38 -0400

    removing square from env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,15 +5,4 @@ sed "s|__NR_LICENSE_KEY__|${NR_KEY}|g" \
   /templates/nr-config.js.template \
   > /usr/share/nginx/html/nr-config.js
 
-# Inject the Square Web Payments SDK URL based on environment.
-# SQUARE_ENVIRONMENT=production  → production CDN
-# anything else (sandbox/unset)  → sandbox CDN
-if [ "${SQUARE_ENVIRONMENT:-sandbox}" = "production" ]; then
-  SQUARE_SDK_URL="https://web.squarecdn.com/v1/square.js"
-else
-  SQUARE_SDK_URL="https://sandbox.web.squarecdn.com/v1/square.js"
-fi
-sed -i "s|__SQUARE_SDK_URL__|${SQUARE_SDK_URL}|g" \
-  /usr/share/nginx/html/index.html
-
 exec nginx -g "daemon off;"
